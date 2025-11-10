a99dddf59ee855d62fef6bcf4515528a9d9e6c09
Olamartins1 <adewoleolaiya@gmail.com>
2025-11-11 00:33:45 +0100

    martin commiting

--- src/pages/IndividualDashboard/Profile/MyNotification.jsx
+++ src/pages/IndividualDashboard/Profile/MyNotification.jsx
@@ -1,7 +1,23 @@
 import React from "react";
+import styled from "styled-components";
 
 const MyNotification = () => {
-  return <div>MyNotification</div>;
+  return (
+    <Container>
+      <Wrapper>akakakka</Wrapper>
+    </Container>
+  );
 };
 
 export default MyNotification;
+
+const Container = styled.div`
+  width: 100%;
+  height: 100%;
+`;
+
+const Wrapper = styled.div`
+  width: 100%;
+  height: 90%;
+  background-color: red;
+`;
